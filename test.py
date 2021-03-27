59d3944836bb1e93060cb7b5bd68c89329e13787
Arthur Stephan <arthurstephan13@gmail.com>
2021-03-27 16:39:45 -0300

    feature: implementation of the test that validates if the movement does not leave the board

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from main import possible_movements
+from main import possible_movements, movement_validation_within_board
 
 
 def test_possible_movements():
@@ -10,3 +10,10 @@ def test_possible_movements():
     assert [-1, 2] == possible_movements([1, 1])[5]
     assert [3, 0] == possible_movements([1, 1])[6]
     assert [3, 2] == possible_movements([1, 1])[7]
+
+
+def test_movement_validation_within_board():
+    assert movement_validation_within_board([1, 3])
+    assert not movement_validation_within_board([-3, 2])
+    assert not movement_validation_within_board([3, -2])
+    assert not movement_validation_within_board([-3, -5])
