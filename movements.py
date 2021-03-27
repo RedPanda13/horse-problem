bd30699546ae69e5448bd88eaf4198d1b72103d1
Arthur Stephan <arthurstephan13@gmail.com>
2021-03-27 17:06:25 -0300

    feature: separation of the function that generates the movements

---
+++ movements.py
@@ -0,0 +1,19 @@
+def possible_movements(current_position):
+    movement_up_right = [current_position[0] - 2, current_position[1] + 1]
+    movement_up_left = [current_position[0] - 2, current_position[1] - 1]
+
+    movement_down_right = [current_position[0] + 2, current_position[1] + 1]
+    movement_down_left = [current_position[0] + 2, current_position[1] - 1]
+
+    movement_left_up = [current_position[1] - 2, current_position[0] - 1]
+    movement_left_down = [current_position[1] - 2, current_position[0] + 1]
+
+    movement_right_up = [current_position[1] + 2, current_position[0] - 1]
+    movement_right_down = [current_position[1] + 2, current_position[0] + 1]
+
+    movements = [
+            movement_up_right, movement_up_left, movement_down_right, movement_down_left,
+            movement_left_up, movement_left_down, movement_right_up, movement_right_down
+           ]
+
+    return movements
