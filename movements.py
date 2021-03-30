329ba7719f69658538630009794a0ffaab5e2b78
Arthur Stephan <arthurstephan13@gmail.com>
2021-03-30 01:09:05 -0300

    fix: changing the name of functions and variables

--- movements.py
+++ movements.py
@@ -14,14 +14,14 @@ def possible_movements(current_position):
     movement_right_up = [current_position[1] + 2, current_position[0] - 1]
     movement_right_down = [current_position[1] + 2, current_position[0] + 1]
 
-    possible_movements = [
+    movements = [
                     ['movement_up_right', movement_up_right], ['movement_down_right', movement_down_right],
                     ['movement_right_up', movement_right_up], ['movement_right_down', movement_right_down],
                     ['movement_up_left', movement_up_left], ['movement_down_left', movement_down_left],
                     ['movement_left_up', movement_left_up], ['movement_left_down', movement_left_down],
                 ]
 
-    return possible_movements
+    return movements
 
 
 def get_random_movement(movements):
--- validations.py
+++ validations.py
@@ -1,19 +1,19 @@
-def movement_validation_within_board(movement):
-    valid_move = True if 0 < movement[1][0] < 7 and 0 < movement[1][1] < 7 else False
+def movement_within_board(movement):
+    within_board = True if 0 < movement[1][0] < 7 and 0 < movement[1][1] < 7 else False
 
-    return valid_move
+    return within_board
 
 
-def validation_visited_squares(movement, visited_squares):
-    valid_move = True if movement[1] not in visited_squares else False
+def visited_squares(movement, visited_squares):
+    not_visited_squares = True if movement[1] not in visited_squares else False
 
-    return valid_move
+    return not_visited_squares
 
 
 def valid_move(movement, visited_squares):
-    within_board = movement_validation_within_board(movement)
-    visited_squares = validation_visited_squares(movement, visited_squares)
+    within_board = movement_within_board(movement)
+    not_visited_squares = visited_squares(movement, visited_squares)
 
-    valid_movement = True if within_board and visited_squares else False
+    valid_movement = True if within_board and not_visited_squares else False
 
     return valid_movement
