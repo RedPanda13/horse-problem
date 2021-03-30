1da2abede679fc9ab051aa7e987f0d7b8a974101
Arthur Stephan <arthurstephan13@gmail.com>
2021-03-29 23:55:45 -0300

    feature: update of comparison form

--- validations.py
+++ validations.py
@@ -1,16 +1,15 @@
 def movement_validation_within_board(movement):
     valid_move = True
 
-    if movement[0] < 0 or movement[0] > 7:
+    if 7 > movement[1][0] < 0:
         valid_move = False
-
-    if movement[1] < 0 or movement[1] > 7:
+    if 7 > movement[1][1] < 0:
         valid_move = False
 
     return valid_move
 
 
 def validation_visited_squares(movement, visited_squares):
-    valid_move = True if movement not in visited_squares else False
+    valid_move = True if movement[1] not in visited_squares else False
 
     return valid_move
