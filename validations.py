50a6758f8f97aa8118132e44c6bd0a47128fcf52
Arthur Stephan <arthurstephan13@gmail.com>
2021-03-30 01:05:04 -0300

    feature: creation of the function that defines if the chosen movement is valid

--- validations.py
+++ validations.py
@@ -8,3 +8,12 @@ def validation_visited_squares(movement, visited_squares):
     valid_move = True if movement[1] not in visited_squares else False
 
     return valid_move
+
+
+def valid_move(movement, visited_squares):
+    within_board = movement_validation_within_board(movement)
+    visited_squares = validation_visited_squares(movement, visited_squares)
+
+    valid_movement = True if within_board and visited_squares else False
+
+    return valid_movement
