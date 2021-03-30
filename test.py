9bd81e51f6e58082827345bc891674ff8ed1b4cf
Arthur Stephan <arthurstephan13@gmail.com>
2021-03-30 00:29:48 -0300

    feature: creation of the test for the function of taking a random movement from the list

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from movements import possible_movements
+from movements import possible_movements,get_random_movement
 from validations import movement_validation_within_board, validation_visited_squares
 
 
@@ -16,7 +16,7 @@ def test_possible_movements():
 def test_movement_validation_within_board():
     assert movement_validation_within_board(['movement_up_right', [1, 3]])
     assert not movement_validation_within_board(['movement_down_left', [-3, 2]])
-        assert not movement_validation_within_board(['movement_left_up', [3, -2]])
+    assert not movement_validation_within_board(['movement_left_up', [3, -2]])
     assert not movement_validation_within_board(['movement_down_right', [-3, -5]])
 
 
@@ -24,3 +24,12 @@ def test_validation_visited_squares():
     assert validation_visited_squares(['movement_up_right', [3, 2]], [[2, 4], [1, 2]])
     assert not validation_visited_squares(['movement_down_left', [1, 2]], [[2, 4], [1, 2]])
     assert not validation_visited_squares(['movement_left_up', [3, 4]], [[2, 4], [1, 2], [3, 4], [4, 2]])
+
+
+def test_get_random_movement():
+    assert 2 == len(get_random_movement([
+                                            ['movement_up_right', [2, 4]], ['movement_down_right', [1, 2]],
+                                            ['movement_right_up', [2, 4]], ['movement_right_down', [1, 2]],
+                                            ['movement_up_left', [3, 4]], ['movement_down_left', [4, 2]],
+                                            ['movement_left_up', [2, 4]], ['movement_left_down', [4, 2]]
+                                        ]))
