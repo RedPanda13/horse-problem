a0ac16e9dc35bbfb466253bac001843a7631d22a
Arthur Stephan <arthurstephan13@gmail.com>
2021-03-31 13:20:39 -0300

    fix: expected results

--- test.py
+++ test.py
@@ -7,12 +7,12 @@ visited_squares = [[2, 4], [1, 2], [3, 4], [4, 2]]
 def test_possible_movements():
     assert [-1, 2] == possible_movements([1, 1])[0]
     assert [3, 2] == possible_movements([1, 1])[1]
-    assert [3, 0] == possible_movements([1, 1])[2]
-    assert [3, 2] == possible_movements([1, 1])[3]
+    assert [0, 3] == possible_movements([1, 1])[2]
+    assert [2, 3] == possible_movements([1, 1])[3]
     assert [-1, 0] == possible_movements([1, 1])[4]
     assert [3, 0] == possible_movements([1, 1])[5]
-    assert [-1, 0] == possible_movements([1, 1])[6]
-    assert [-1, 2] == possible_movements([1, 1])[7]
+    assert [0, -1] == possible_movements([1, 1])[6]
+    assert [2, -1] == possible_movements([1, 1])[7]
 
 
 def test_movement_within_board():
