009fed768305125178ad784e29a6ae052d476907
Arthur Stephan <arthurstephan13@gmail.com>
2021-03-31 13:21:31 -0300

    fix: check if the position is inside the board

--- validations.py
+++ validations.py
@@ -1,5 +1,11 @@
 def movement_within_board(movement):
-    within_board = True if 0 < movement[0] < 7 and 0 < movement[1] < 7 else False
+    within_board = True
+
+    if movement[0] < 0 or movement[0] > 7:
+        within_board = False
+
+    if movement[1] < 0 or movement[1] > 7:
+        within_board = False
 
     return within_board
 
